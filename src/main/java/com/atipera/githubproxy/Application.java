d2d3541e1f5321c6e33b71e1e3685501734cefbc
adrian.szponar <adrian.szponar7@o2.pl>
2026-05-28 16:49:48 +0200

    Initial commit: Setup Spring Boot 4 project

---
+++ src/main/java/com/atipera/githubproxy/Application.java
@@ -0,0 +1,13 @@
+package com.atipera.githubproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
